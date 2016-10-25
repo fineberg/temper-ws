e96eb1860fa5cd9f762559b34a6354ba7ee8f66f
U-Sams-SixCore\Sam <sam@fineberg.net>
2016-10-25 13:13:40 -0400

    Imported temps into python dictionaries

--- read-temps.py
+++ read-temps.py
@@ -1,10 +1,16 @@
 
 import requests
+import json
 
 link = "http://attic.fineberg.net:33433"
 f = requests.get(link)
 print "Attic:\n", f.text
+attic = json.loads(f.text)
+
 link = "http://basement.fineberg.net:33433"
 f = requests.get(link)
-
 print "Basement:\n", f.text
+basement = json.loads(f.text)
+
+print "Attic temp: ", attic['temperature']
+print "Basement temp", basement['temperature']
