0e15bac437b7b5579c4e2fc40c6bc2c80d71f502
fineberg <sam@wind.fineberg.net>
2016-10-31 09:00:32 -0400

    Added "some" error handling and retry to the track temps script

--- track-temps.py
+++ track-temps.py
@@ -4,15 +4,44 @@ import json
 import subprocess
 import datetime
 
-link = "http://attic.fineberg.net:33433"
-f = requests.get(link)
-#print "Attic:\n", f.text
-attic = json.loads(f.text)
+errors = 0
+ok = False
+while (not ok) and (errors < 10) :
+   link = "http://attic.fineberg.net:33433"
+   try :
+      f  = requests.get(link)
+#      print "Attic:\n", f.text
+      attic = json.loads(f.text)
+      if (attic['humidity'] < 0) or (attic['tempf'] < 0): 
+         ok=false
+      else: 
+         ok = True
 
-link = "http://basement.fineberg.net:33433"
-f = requests.get(link)
-#print "Basement:\n", f.text
-basement = json.loads(f.text)
+      break
+   except:
+      print "try again... Attic:\n", f.text
+      errors = errors + 1
+  
+if (errors >= 10): raise
+ 
+ok = False
+errors = 0
+while (not ok) and (errors < 10) :
+   link = "http://basement.fineberg.net:33433"
+   try: 
+      f = requests.get(link)
+#      print "Basement:\n", f.text
+      basement = json.loads(f.text)
+      if (basement['humidity'] < 0) or (basement['tempf'] < 0): 
+         ok=false
+      else: 
+         ok = True
+      break	
+   except:
+      print "try again... basement:\n", f.text
+      errors = errors + 1
+
+if (errors >= 10): raise
 
 string = datetime.datetime.now().isoformat()
 
